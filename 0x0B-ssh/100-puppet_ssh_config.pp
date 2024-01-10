b08a98525d46ef11f52b20d8113d030603cf53e0
Uduak Umanah <eyakenojnr@gmail.com>
2024-01-10 23:27:23 +0100

    Client configuration file with Puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Setting up client config file
+
+file_line { 'Turn off passwd auth':
+	path   => '/etc/ssh/ssh_config',
+	line   => 'PasswordAuthentication no',
+	match  => '^#?PasswordAuthentication',
+	ensure => present,
+}
+
+file_line { 'Declare identity file':
+	path   => '/etc/ssh/ssh_config',
+	line   => 'IdentityFile ~/.ssh/school',
+	ensure => present,
+}

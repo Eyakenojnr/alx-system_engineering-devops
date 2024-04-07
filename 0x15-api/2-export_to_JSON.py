54d6a2db74bc6a5c3bb36e99bec86f62ba763790
Uduak Umanah <eyakenojnr@gmail.com>
2024-04-07 01:03:44 +0100

    Export data in JSON format

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
+"""Exports to-do list information for a given employee ID in JSON format."""
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
+    ID = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+    # GET employee data whose userID is passed to the script
+    users = requests.get(f"{url}/users/{ID}").json()
+    todos = requests.get(f"{url}/todos?userId={ID}").json()
+
+    # Prepare json data to export
+    export_data = {"USER_ID": []}
+    for task in todos:
+        task_data = {
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': users.get('name')
+        }
+        export_data["USER_ID"].append(task_data)
+
+    # Export data to JSON file
+    json_file = f"{ID}.json"
+    with open(json_file, "w") as file:
+        json.dump(export_data, file)
